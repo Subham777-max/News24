dd4248f1d888d377460d8c4f6bada5ca839860a5
Subham777-max <subhamray865@gmail.com>
2025-10-08 23:14:52 +0530

    tying to fix

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
-export const axiosInstance=axios.create({
-    baseURL:`${CORS_PROXY}https://gnews.io/api/v4`
-})
+
+const PROXY = "https://api.allorigins.win/raw?url=";
+
+export const axiosInstance = axios.create({
+  baseURL: PROXY + encodeURIComponent("https://gnews.io/api/v4"),
+});
